d02fca16c6c7eebfd75bbe23ba57e466e5741434
SUJAY47 <sujay.pmna@gmail.com>
2020-07-27 02:34:40 +0530

    Updates

---
+++ precache-manifest.4c435b87a828951a47c1f8d92a40cd3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4eddfef04cd355059fd1c956ddbe18a",
+    "url": "/react-post-app/index.html"
+  },
+  {
+    "revision": "1424e2bd6c4cc677f24b",
+    "url": "/react-post-app/static/css/main.24578f04.chunk.css"
+  },
+  {
+    "revision": "7122330d2120008193da",
+    "url": "/react-post-app/static/js/2.5435825d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-post-app/static/js/2.5435825d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1424e2bd6c4cc677f24b",
+    "url": "/react-post-app/static/js/main.7277d5bb.chunk.js"
+  },
+  {
+    "revision": "edde575e7efb9c4159c4",
+    "url": "/react-post-app/static/js/runtime-main.e7cc85ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-post-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
